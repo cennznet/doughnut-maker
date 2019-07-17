c5d49e5d61f0eb904266449cfaa5ca95e8b69327
holygits <beauchjord@gmail.com>
2019-07-17 13:14:59 +1200

    In-line wasm ready check for schnorrkel signing

--- src/doughnut/signingMethods/0/index.js
+++ src/doughnut/signingMethods/0/index.js
@@ -5,9 +5,23 @@ const {
   schnorrkelSign,
   schnorrkelVerify,
 } = require("@polkadot/util-crypto");
+const { isReady } = require('@polkadot/wasm-crypto');
+
+function wait_ms(ms) {
+  var start = Date.now();
+  while (Date.now() - start < ms) {}
+}
+
+// Wrap schnorrkel signing to await wasm crypto readiness
+function sign(payload, keypair) {
+    for(i = 0; i < 10; i++) {
+      if(!isReady()) wait_ms(10);
+    }
+    return schnorrkelSign(payload, keypair);
+}
 
 module.exports = {
-  sign: schnorrkelSign,
+  sign: sign,
   verify: schnorrkelVerify,
   separate(doughnut) {
     return [
