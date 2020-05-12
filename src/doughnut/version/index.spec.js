1e0113bc2adee244762ee460446f8fc93bd6d157
hoani <hoani.bryson@gmail.com>
2020-05-13 08:18:29 +1200

    Correct version encoding

--- src/doughnut/version/index.spec.js
+++ src/doughnut/version/index.spec.js
@@ -4,6 +4,19 @@
 const versionEncoding = require("./");
 
 describe("Version Encoding", () => {
+  it("Version encodes as a LE u16", () => {
+    const payloadVersion = 0x0a;
+    const signingMethod = 0x05;
+
+    const versionBinary =
+      versionEncoding.encode(
+        payloadVersion,
+        signingMethod
+    );
+
+    expect(versionBinary).toEqual(new Uint8Array([0x0a, 0x05 << 3]));
+  });
+
   it("should generate and separate cleanly", () => {
     const payloadVersion = 2000;
     const signingMethod = 25;
